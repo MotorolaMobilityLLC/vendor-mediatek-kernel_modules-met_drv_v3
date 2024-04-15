0f3a2cb5d14762bef07c1f7631c6e871c88055a8
Doris Kao <Doris.Yhkao@mediatek.com>
2024-04-15 13:20:47 +0800

    [ALPS08753548] MET : check mcupm reserve memory api exist
    [Description]
    check api exist then call the function avoid function phase-out and met.ko insmod fail
    
    [Test]
    pass
    
    MTK-Commit-Id: 17bc3feee980f72297151da36a924d8fb2e5bd15
    
    Signed-off-by: Doris Kao <Doris.Yhkao@mediatek.com>
    CR-Id: ALPS08753548
    Feature: MET Driver
    Change-Id: Ic311929315b58b343caa640c8efe40156994d8b2

--- common/tinysys/v1/sspm/sspm_met_log.c
+++ common/tinysys/v1/sspm/sspm_met_log.c
@@ -240,9 +240,27 @@ int sspm_log_init(struct device *dev)
 			sspm_buf_available = 0;
 		}
 #elif defined(SSPM_VERSION_V0) || defined(SSPM_VERSION_V1) || defined(SSPM_VERSION_V2)
-		sspm_log_virt_addr = (void*)sspm_reserve_mem_get_virt(MET_MEM_ID);
-		sspm_log_phy_addr = sspm_reserve_mem_get_phys(MET_MEM_ID);
-		sspm_buffer_size = sspm_reserve_mem_get_size(MET_MEM_ID);
+		if(sspm_reserve_mem_get_virt_symbol){
+			sspm_log_virt_addr = (void*)sspm_reserve_mem_get_virt_symbol(MET_MEM_ID);
+			PR_BOOTMSG("sspm_log_virt_addr=%p\n", sspm_log_virt_addr);
+		}else {
+			PR_BOOTMSG("[MET] [%s,%d] sspm_reserve_mem_get_virt is not linked!\n", __FILE__, __LINE__);
+			return -1;
+		}
+		if(sspm_reserve_mem_get_phys_symbol){ 
+			sspm_log_phy_addr = sspm_reserve_mem_get_phys_symbol(MET_MEM_ID);
+			PR_BOOTMSG("sspm_log_phy_addr=%u\n", (unsigned int) sspm_log_phy_addr);
+		}else {
+			PR_BOOTMSG("[MET] [%s,%d] sspm_reserve_mem_get_phys is not linked!\n", __FILE__ ,__LINE__);
+			return -1;
+		}
+		if(sspm_reserve_mem_get_size_symbol){
+			sspm_buffer_size = sspm_reserve_mem_get_size_symbol(MET_MEM_ID);
+			PR_BOOTMSG("sspm_buffer_size=%x\n", sspm_buffer_size);
+		}else {
+			PR_BOOTMSG("[MET] [%s,%d] sspm_reserve_mem_get_size is not linked!\n", __FILE__, __LINE__);
+			return -1;
+}		
 
 		if ((sspm_log_phy_addr > 0) && (sspm_buffer_size > 0)) {
 			sspm_buf_available = 1;
